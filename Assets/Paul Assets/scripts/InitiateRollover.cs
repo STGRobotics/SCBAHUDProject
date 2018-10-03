7efe439875ef0b1c25a877f72082e1a5cfdbe0d8
Paul Merritt <paul.merritt@nist.gov>
2018-10-03 11:56:00 -0600

    adjusted collider for gaze detection and added text instructions

--- Assets/Paul Assets/scripts/InitiateRollover.cs
+++ Assets/Paul Assets/scripts/InitiateRollover.cs
@@ -5,11 +5,11 @@ using HoloToolkit.Unity.InputModule;
 
 public class InitiateRollover : MonoBehaviour {
 
-	public GameObject manager;
-	public GameObject target;
-	public GameObject evacText;
-	public GameObject evacPath;
-	private bool evac;
+	private GameObject manager;
+	private GameObject target;
+	private GameObject evacText;
+	private GameObject evacPath;
+	private GameObject player;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +17,7 @@ public class InitiateRollover : MonoBehaviour {
 		target = manager.GetComponent<GazeManager>().HitObject;
 		evacText = GameObject.FindWithTag("EvacText");
 		evacPath = GameObject.FindWithTag("EvacPath");
+		player = GameObject.Find("MixedRealityCamera");
 		evacText.SetActive(false);
 		evacPath.SetActive(false);
 	}
@@ -24,16 +25,19 @@ public class InitiateRollover : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		target = manager.GetComponent<GazeManager>().HitObject;
-		if (target == gameObject){
+		if (target != null){
+			//Debug.Log(target);
+		}
+		if (target == GameObject.Find("Cube")){
 			Rollover();
 		}
 	}
 
 	void Rollover(){
-		Debug.Log("INITIATED");
+		Debug.Log("INITIATED ROLLOVER");
 		evacText.SetActive(true);
-		Debug.Log("one");
+		//Debug.Log("one");
 		evacPath.SetActive(true);
-		Debug.Log("done");
+		//Debug.Log("done");
 	}
 }
--- Assets/Paul Assets/scripts/TemperatureScanner.cs
+++ Assets/Paul Assets/scripts/TemperatureScanner.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 
 public class TemperatureScanner : MonoBehaviour {
 
-	public GameObject player;
-	public GameObject fire;
+	private GameObject player;
+	private GameObject fire;
 	public float temp = 0;
 
 	private Vector3 pos1;
 	private Vector3 pos2;
 	private float dist;
+	private GameObject fireText;
 
 	Image fillImg;
 
@@ -19,21 +20,34 @@ public class TemperatureScanner : MonoBehaviour {
 	void Start () {
 		player = GameObject.Find("MixedRealityCamera");
 		fire = GameObject.Find("Fire");
+		fireText = GameObject.FindWithTag("FireText");
+		fireText.SetActive(false);
 		pos1 = player.transform.position;
 		pos2 = fire.transform.position;
 		dist = 1;
 		fillImg = this.GetComponent<Image>();
+		fire.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		pos1 = player.transform.position;
-		pos2 = fire.transform.position;
 		dist = Vector3.Distance(pos1, pos2);
 		//Debug.Log(dist + " meters");
 		//Debug.Log(temp + " degrees");
 
 		temp = 300-dist;
+
+		if (dist < 75){
+			fire.SetActive(true);
+			if (GameObject.FindWithTag("EvacText")){
+				fireText.SetActive(false);
+			}
+			else {
+				fireText.SetActive(true);
+			}
+			
+		}
 
 		if (temp < 300)
 		{
